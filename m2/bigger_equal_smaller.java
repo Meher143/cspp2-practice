958093546bccbfcbfa764abc671f4600b6f5a56c
Meher143 <krishmeher@msitprogram.net>
2018-08-28 13:56:06 +0530

    biggerequal

---
+++ m2/bigger_equal_smaller.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+class bigger_equal_smaller{
+	public static void main(String[] args){
+		Scanner scan = new Scanner(System.in);
+		int VarA = scan.nextInt();
+		int VarB = scan.nextInt();
+		if(VarA>VarB){
+			System.out.println("Bigger");
+		}
+			else if(VarA == VarB){
+				System.out.println("Equal");
+}				else{
+					System.out.println("Smaller");
+
+				}
+			}
+
+		}
+	
