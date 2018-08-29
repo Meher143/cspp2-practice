f0c4e186fb71aef4f79b0d003e1e80e1f9439030
Meher143 <krishmeher@msitprogram.net>
2018-08-29 14:16:07 +0530

    goodbye

---
+++ m3/ConverttoWhileLoop.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+class convertwhileloop {
+	public static void main (final String[] args) {
+		int n = 0;
+		while(n<10){
+		n = n+2;
+		System.out.println(n);
+	}
+	
+		System.out.println("GoodBye!");
+}
+}
